cfb54b45c4e3073d667a8c39f997595e122446b1
Vensan <v.drot@student.inha.uz>
2022-01-08 23:23:30 +0500

    First page addoptation

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -1,6 +1,10 @@
 const hamburger = document.getElementById('hamburger');
 const navul = document.getElementById('nav-ul');
+const navbar = document.getElementById('navh');
 
 hamburger.addEventListener('click', () => {
-    navul.classList.toggle('show');
+    navh.classList.toggle('active');
+    navul.classList.toggle('active');
+    hamburger.classList.toggle('active');
+
 });
