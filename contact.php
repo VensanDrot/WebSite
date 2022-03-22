4e51eabba29791d4a1cf77fcaeac78ca2ddc38a5
Vensan <v.drot@student.inha.uz>
2022-03-23 00:25:58 +0500

    finished

--- contact.php
+++ contact.php
@@ -8,16 +8,16 @@
         $email = trim(htmlspecialchars(strip_tags($_POST['email']))); 
         $message = trim(htmlspecialchars(strip_tags($_POST['message'])));
         $email = filter_var($email, FILTER_VALIDATE_EMAIL); 
-        
-        printf($name);
-        printf( $message);
-        printf($email);
+  
+        $con = array (true,true,true,false);
+       
+
 
         if(!empty($name)){
             $last_name = $name;
         }else{
             $error_name = "Enter the name";
-          
+            $con[0]=false; 
         }
         
         
@@ -25,25 +25,39 @@
             $last_email = $email;
         }else{
             $error_email = "Enter the email";
-        
+            $con[1]=false; 
+            
         }
         
         
         
         if(!empty($message)){
             $last_message = $message;
+            
         }else{
             $error_message = "Enter the message";
+            $con[2]=false; 
         }
+
+        if (isset($error_message) || isset($error_name) || isset($error_email)){
+            if (isset($error_message)) {
+              
+            }
+            if(isset($error_email)) {
+                
+            }
+            if(isset($error_name)) {
+               
+            }
+        } 
             
         
         
         if(isset($last_name) && isset($last_email) && isset($last_message)){
             $insert = mysqli_query($connect, "INSERT INTO `messages`(`name`, `email`, `text`) VALUES ('$last_name','$last_email','$last_message')");
-            echo($error_message);
-            header("Location: ?#contact");
+           $con[3]=true;   
         }
 
+       echo json_encode($con);
         
-    
         ?>
---
+++ public/php/connect.php
@@ -0,0 +1,13 @@
+
+<?
+$connect = mysqli_connect("localhost","root","root","contact");
+
+// Check connection
+if ($connect->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+
+?>
